52476055d94ebe32f4bb27f7c666b6ad948b6aaa
PAPITO <45469370+ometere123@users.noreply.github.com>
2026-06-27 21:32:39 +0100

    Update contract address to 0x8E71...8889

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = '0x78EE27e181A5f8B4fAc6BaE96DdB9AffD9249d48' as const;
+export const CONTRACT_ADDRESS = '0x8E712fE53595Bd4d9fc3858199E07E1d74e08889' as const;
 
 export const STUDIONET_CONFIG = {
   chainId: 61999,
